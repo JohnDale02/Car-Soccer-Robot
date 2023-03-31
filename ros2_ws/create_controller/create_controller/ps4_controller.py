2cb24c7fb582e20a1a1347e7d895774ed066b1d2
Thomas Groechel <tgroechel@irobot.com>
2023-03-31 16:53:42 -0500

    pwm signal send code

--- ros2_ws/create_controller/create_controller/ps4_controller.py
+++ ros2_ws/create_controller/create_controller/ps4_controller.py
@@ -1,4 +1,10 @@
 from pyPS4Controller.controller import Controller
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)			#disable warnings
+GPIO.setmode(GPIO.BOARD)		#set pin numbering system
+GPIO.setup(ledpin,GPIO.OUT)
 
 class PS4Controller(Controller):
     def __init__(self, **kwargs):
@@ -7,6 +13,13 @@ class PS4Controller(Controller):
         self.cmd_led_pub_cb = None
         self.x = 0
         self.y = 0
+
+        # pwm
+        self.l_paddle_pin = 12 # set to whatever the left bumper pin is
+        self.r_paddle_pin = 14 # set to whatever the right bumper pin is
+        self.l_pi_pwm = GPIO.PWM(self.l_paddle_pin,1000) #create PWM instance with frequency
+        self.r_pi_pwm = GPIO.PWM(self.r_paddle_pin,1000) #create PWM instance with frequency
+
     
     # ROS callbacks
     def register_cmd_vel_pub_cb(self, callback_func):
@@ -20,6 +33,7 @@ class PS4Controller(Controller):
 
     # Listen
     def spin_controller(self):
+        self.pi_pwm.start(0)				#start PWM of required Duty Cycle 
         self.listen(on_connect=self.connected, on_disconnect=self.disconnected)
 
     # Controller events
@@ -55,7 +69,7 @@ class PS4Controller(Controller):
         return (float(value) / 32767.0) * 0.46
 
     def map_motor(self,value):
-        return value // 100
+        return abs(float(value) / 32767.0 * 75)
 
     # Actions
     def move_robot_X(self,value):
@@ -70,11 +84,14 @@ class PS4Controller(Controller):
 
     def send_pwm_left(self,value):		# Function to send PWM FLIPPER signals to GPIO pin for Flipper on left (PIN y)
         value = self.map_motor(value)
-        print(f"LT: {value}")
+        print(f"LT duty value: {value}")
+        self.l_pi_pwm.ChangeDutyCycle(value)
+
 
     def send_pwm_right(self,value):		# Function to send PWM FLIPPER signals to GPIO pin for Flipper on right(PIN x)
         value = self.map_motor(value)
-        print(f"RT: {value}")
+        print(f"RT duty value: {value}")
+        self.r_pi_pwm.ChangeDutyCycle(value)
     
     def reset_flippers(self):  # Function to slowly rotate flippers back so they can synchronize for next shot (Good for Accuracy)
         pass
