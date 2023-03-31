54750ad62e22462035b229e50cc947b14c076897
Thomas Groechel <tgroechel@irobot.com>
2023-03-31 16:35:21 -0500

    rainbow led

--- ros2_ws/create_controller/create_controller/create_controller.py
+++ ros2_ws/create_controller/create_controller/create_controller.py
@@ -9,12 +9,17 @@ from geometry_msgs.msg import Twist
 from irobot_create_msgs.msg import LightringLeds
 from irobot_create_msgs.msg import LedColor
 
+import math, time
+
 class CreateControllerPub(Node):
     def __init__(self, controller):
         super().__init__("create_controller_pub")
         self.twist_publisher = self.create_publisher(Twist, "cmd_vel", 10)
         self.led_publisher = self.create_publisher(LightringLeds, "cmd_lightring", 10)
         
+        self.run_rainbow = False
+        self.rainbow_thread = None
+
         self.twist_msg = Twist()
         self.led_msg = LightringLeds()
         
@@ -24,7 +29,7 @@ class CreateControllerPub(Node):
         
     def start_up_controller(self):
         self.controller.register_cmd_vel_pub_cb(self.publish_twist_msg)
-        self.controller.register_cmd_led_pub_cb(self.publish_led)
+        self.controller.register_cmd_led_pub_cb(self.toggle_rainbow)
         self.ct = threading.Thread(target=self.controller.spin_controller)
         self.ct.start()
     
@@ -33,15 +38,36 @@ class CreateControllerPub(Node):
         self.twist_msg.angular.z = float(turn)
         self.twist_publisher.publish(self.twist_msg)
     
-    def publish_led(self):
-        rand_color_led = LedColor()
-        rand_color_led.red = random.randint(0,255)
-        rand_color_led.green = random.randint(0,255)
-        rand_color_led.red = random.randint(0,255)
+    def toggle_rainbow(self):
+        self.run_rainbow = not self.run_rainbow
+        if self.run_rainbow:
+            self.rainbow_thread = threading.Thread(target=self.circle_rainbow)
+            self.rainbow_thread.start()
+        elif self.rainbow_thread != None:
+            self.rainbow_thread.stop()
+            self.rainbow_thread = None
 
-        for i in range(0,6):
-            self.led_msg.leds[i] = rand_color_led
-        
+    # https://github.com/ipekgoktan/SHINE_Ipek-Mena/blob/master/src/kuri_mi/src/kuri_light.py
+    def circle_rainbow(self):
+        # Tunable rainbow params
+        center = 128
+        width = 127
+        length = 50
+        f1 = f2 = f3 = 0.3
+        p1 = 0
+        p2 = 2
+        p3 = 4
+
+        # rainbow pattern
+        for i in range(length):
+            for p in range(len(self.led_msg.leds)):
+                self.led_msg.leds[p].red = math.sin(f1*i + p1) * width + center
+                self.led_msg.leds[p].green = math.sin(f2*i + p2) * width + center
+                self.led_msg.leds[p].blue = math.sin(f3*i + p3) * width + center
+            self.publish_led()
+            time.sleep(0.1)
+
+    def publish_led(self):
         self.led_publisher.publish(self.led_msg)
 
 
