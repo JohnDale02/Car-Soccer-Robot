796b31938f61024621a625e6a54803f63b9f4861
Thomas Groechel <tgroechel@irobot.com>
2023-03-31 10:51:59 -0500

    dfsklj

--- ros2_ws/create_controller/create_controller/ps4_controller.py
+++ ros2_ws/create_controller/create_controller/ps4_controller.py
@@ -52,7 +52,7 @@ class PS4Controller(Controller):
     
     # Mapping
     def map_movement(self,value):  # function that maps raw joystick data  --> create angle
-        return (value / 32767) * 0.46
+        return (float(value) / 32767.0) * 0.46
 
     def map_motor(self,value):
         return value // 100
